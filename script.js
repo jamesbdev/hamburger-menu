2d028bc4ac1ba0e4ba0843767430bfd3a75b7807
James Bremner <j.bremner@ymail.com>
2025-02-19 15:41:48 +0000

    add close icon

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-console.log('Hello, world!');
+// Purpose: This file is used to handle the burger menu functionality
 
+const openBtn = document.querySelector('.burger');
+const menuContainer = document.querySelector('.nav-links-container');
+const closeBtn = document.querySelector('.close');
+
+console.log(menuContainer);
+
+openBtn.addEventListener('click', (event) => {
+  menuContainer.classList.remove("hide");
+})
+
+closeBtn.addEventListener('click', (event) => {
+    menuContainer.classList.add("hide");
+});
